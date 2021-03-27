0bc35fc2d76ad7e3a2375cd84b7484de44e558d6
Dax Hurley <dax.orion.hurley@gmail.com>
2021-03-26 19:14:02 -0600

    Classwork on Mongodb

---
+++ controllers/subscribersController.js
@@ -0,0 +1,34 @@
+const subscriber = require("../models/subscriber");
+const Subscriber = require("../models/subscriber");
+
+exports.getAllSubscribers = (req, res) => {
+    Subscriber.find({})
+        .exec()
+        .then(subscribers => {
+            res.render("subscribers", {subscribers: subscribers});
+        })
+        .catch((error) => {
+            console.log(error);
+            return [];
+        })
+        .then(() => {
+            console.log("promise complete");
+        });
+};
+
+exports.getSubscriptionPage = (req, res) => {
+    res.render("contacts");
+}
+
+exports.saveSubscriber = (req, res) => {
+    let newSubscriber = new Subscriber({
+        name: req.body.name,
+        email: req.body.email,
+        zipCode: req.body.zipCode
+    });
+    newSubscriber.save()
+    .then(() => {
+        res.render("thanks");
+    })
+    .catch(error => { res.send(error) });;
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Classwork on Node.js and Express.js
+
+A simple website for a company offering cooking classes.
+
+# Usage
+
+Before you being, MongoDB should be installed and running on your computer/the server
+
+```
+npm install
+npm start
+```
+
+Note: As of right now, the run script relies on [nodemon](https://www.npmjs.com/package/nodemon) being installed globally, get it with:
+
+```
+npm install -g nodemon
+```
